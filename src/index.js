23c4a09aa636761dbcb5d5e475653937ab0fbf33
smartflat-kimsk <102001812+smartflat-kimsk@users.noreply.github.com>
2022-05-10 15:24:32 +0900

    import

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+
 function Square(props) {
 	return (
 		<button className="square" onClick={props.onClick}>
