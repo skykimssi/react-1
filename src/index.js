af5ee5a9a48aedf594aa4117a60e74a7b08f4b6f
smartflat-kimsk <102001812+smartflat-kimsk@users.noreply.github.com>
2022-05-13 15:05:36 +0900

    이중 반복 랜더링

--- src/index.js
+++ src/index.js
@@ -15,16 +15,43 @@ class Board extends React.Component {
 	renderSquare(i) {
 		return (
 			<Square
+				key={i} //안넣어주면 계속 key 없다고 에러뜸
 				value={this.props.squares[i]}
 				onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
-
+	// return 안에서는 반복문 사용 못함 > map 사용 하던가 return 바깥에서 처리
 	render() {
+		let boardSquares = [];
+		for (let row = 0; row < 3; row++) {
+			let boardRow = [];
+			for (let col = 0; col < 3; col++) {
+				boardRow.push(this.renderSquare((row * 3) + col));
+			}
+			boardSquares.push(<div key={row} className="board-row" >{boardRow}</div>);
+		}
 		return (
 			<div>
-				<div className="board-row">
+				{boardSquares}
+				{/* {
+					Array(3).fill().map((_, row)=>{
+						console.log(row)
+						return(
+							<div key={row} className="board-row">
+							{
+								Array(3).fill().map((_, col)=>{
+									console.log(row*3+col+1)
+									return(
+										this.renderSquare(row*3+col)
+									)
+								})
+							}
+							</div>
+						)
+					})
+				} */}
+				{/* <div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
 					{this.renderSquare(2)}
@@ -38,11 +65,13 @@ class Board extends React.Component {
 					{this.renderSquare(6)}
 					{this.renderSquare(7)}
 					{this.renderSquare(8)}
-				</div>
+				</div> */}
 			</div>
 		);
 	}
 }
+
+
 
 class Game extends React.Component {
 	constructor(props) {
@@ -50,18 +79,18 @@ class Game extends React.Component {
 		this.state = {
 			history: [{
 				squares: Array(9).fill(null),
-				x : null,
-				y : null,
+				x: null,
+				y: null,
 			}],
 			xIsNext: true,
 			stepNumber: 0,
-			
+
 		};
 	}
 
 	handleClick(i) {
-		const history = this.state.history.slice(0,this.state.stepNumber + 1);
-		
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
@@ -72,15 +101,15 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares,
 				//x : parseInt(i/3)+1,
-				x : Math.trunc((i/3)+1),
-				y : (i%3)+1,
+				x: Math.trunc((i / 3) + 1),
+				y: (i % 3) + 1,
 			}]),
 			xIsNext: !this.state.xIsNext,
 			stepNumber: history.length,
 		});
 	}
 
-	jumpTo(step){
+	jumpTo(step) {
 		this.setState({
 			stepNumber: step,
 			xIsNext: (step % 2) === 0,
@@ -91,12 +120,12 @@ class Game extends React.Component {
 		const history = this.state.history;
 		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
-		
+
 		const moves = history.map((step, move, array) => {
 			const desc = move ?
 				//ES6 백틱 사용
 				//'Go to move #' + move + ' ( '+step.x+','+step.y+' )':
-				`Go to move # ${move} ( ${step.x}, ${step.y} )`:
+				`Go to move # ${move} ( ${step.x}, ${step.y} )` :
 				'Go to game start';
 			return (
 				<li key={move}>
